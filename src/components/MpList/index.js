c00c138a3facd166717cd9e793c06713147db8ad
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2024-03-19 20:58:22 +0530

    changesadded

--- src/components/MpList/index.js
+++ src/components/MpList/index.js
@@ -10,7 +10,8 @@ const MpList = () => {
             name:"Gopal Chinnayya Shetty",
             wiki:"https://en.wikipedia.org/wiki/Gopal_Shetty",
             party:"BJP",
-            symbol:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Bharatiya_Janata_Party_logo.svg/270px-Bharatiya_Janata_Party_logo.svg.png"
+            symbol:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Bharatiya_Janata_Party_logo.svg/270px-Bharatiya_Janata_Party_logo.svg.png",
+            summary:"Gopal Chinayya Shetty is a Mangalore based Indian politician. He is a member of parliament from Mumbai North. Previously, he was a 2 terms Member of Legislative Assembly (MLA) from the Borivali Vidhan Sabha constituency and 3 terms Corporator of Brihanmumbai Mahanagar Corporation (BMC) from 1992-2004"
 
         },
         { 
@@ -19,7 +20,8 @@ const MpList = () => {
             name:"Gajanan Chandrakant Kirtikar",
             wiki:"https://en.wikipedia.org/wiki/Gajanan_Kirtikar",
             party:"shivsena",
-            symbol:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Logo_of_Shiv_Sena.svg/270px-Logo_of_Shiv_Sena.svg.png"
+            symbol:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Logo_of_Shiv_Sena.svg/270px-Logo_of_Shiv_Sena.svg.png",
+            summary:'Gajanan Kirtikar is Leader of Shivsena and a Member of Parliament (MP) from Mumbai North West (Lok Sabha constituency) in Mumbai, Maharashtra, India.[1] He has been a Member of Legislative Assembly from Malad assembly constituency in Mumbai from 1990 to 2009'
         },
         { 
             key:3,
@@ -27,7 +29,8 @@ const MpList = () => {
             name:"zeeshan Siddique",
             wiki:"https://en.wikipedia.org/wiki/Zeeshan_Siddique",
             party:"congress",
-            symbol:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Indian_National_Congress_hand_logo.svg/225px-Indian_National_Congress_hand_logo.svg.png"
+            symbol:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Indian_National_Congress_hand_logo.svg/225px-Indian_National_Congress_hand_logo.svg.png",
+            summary:'Zeeshan Baba Siddique (born 3 October 1992) is an Indian politician from the Indian National Congress. He is currently a Member of the Legislative Assembly in Maharashtra Legislative Assembly.[1] & President of Mumbai Youth Congress.'
         }
         
     ]
@@ -45,7 +48,6 @@ const MpList = () => {
                     <MpListItem key={item.id} item={item} />
                 ))}
             </div>
-            
         </div>
     )
 }
--- src/components/MpListItem/index.js
+++ src/components/MpListItem/index.js
@@ -1,4 +1,10 @@
 import {useState} from 'react'
+import { FaSquareTwitter } from "react-icons/fa6";
+import { FaWikipediaW } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { PiTelevisionBold } from "react-icons/pi";
 import './index.css'
 
 const MpListItem = props => {
@@ -8,16 +14,31 @@ const MpListItem = props => {
         setMore({isMore:!isMore})
     }
     return(
+        <div className='mpContainer'>
     <div className="mp">
         <div className="image">
             <img src={item.image} alt={item.name} className="personImage" />
         </div>
         <div className="details">
             <h1 className="name">{item.name}</h1>
-            <button className="moreBtn" onClick={()=>getMore()}>More</button>
+            <button className="moreBtn" onClick={()=>getMore()}>{isMore ? item.party: 'More'}</button>
         </div>
         <div className='partySymbol'>
             <img src={item.symbol} alt={item.party} className='symbol'/>
+        </div>  
+    </div>
+    <div className={isMore ? 'show':'hide'}>
+            <div className='candidateSummary'>
+                {item.summary}
+            </div>
+            <div className='links'>
+               <a href="#" ><FaSquareTwitter className='linkIcon'/></a>
+               <a href="#" ><FaFacebookSquare className='linkIcon'/></a>
+               <a href={item.wiki} ><FaWikipediaW className='linkIcon1'/></a>
+               <a href="#" ><FaInstagramSquare className='linkIcon'/></a>
+               <a href="#" ><AiOutlineYoutube className='linkIcon'/></a>
+               <a href="#" ><PiTelevisionBold className='linkIcon1'/></a>
+            </div>
         </div>
     </div>
 )
