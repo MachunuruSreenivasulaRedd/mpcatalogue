df3563998e5f68de61a17766b48ea70209d8d47b
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2024-03-22 07:47:40 +0530

    changesadded

--- src/components/MpList/index.js
+++ src/components/MpList/index.js
@@ -1,9 +1,15 @@
 import { GoLocation } from "react-icons/go";
 import MpListItem from "../MpListItem";
 import './index.css'
+import {useState} from 'react'
 import ListContext from "../../context/ListContext";
 
-const MpList = () => (
+
+const states=["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala" ,"madhya Pradesh","Maharastra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamilnadu","Telangana","Tripura","UttarPradesh","Uttarakhand","WestBengal","Andaman and NicobarIslands","chandigarh","Lakshadweeep","Dadra and Nagar Haveli and Daman and Diu","Delhi","puducherry","Ladakh"]
+const MpList = () => {
+    const [state, setState] = useState()
+    const [constituency, setconstituency] = useState()
+    return(
         <ListContext.Consumer>
             {value => {
                 const {mpList}=value
@@ -12,6 +18,26 @@ const MpList = () => (
         <div className="listContainer">
             <div className="topContainer">
                 <h1 className="slogan">BharatPol</h1>
+                <div className='input'>
+                                        <label htmlFor="states" className='selection'>State</label>
+                                        <select value={state} id="state" onSelect={e => setState(e.target.value)}>
+                                            {states.map(item => (
+                                                <option value={item}>{item}</option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                    <div className='input'>
+                                        <label htmlFor='constituency' className='selection'>constituency</label>
+                                        <select value={constituency} id="constituency" onSelect={e => setconstituency(e.target.value)}>
+                                            <option value="North Mumbai">North Mumbai</option>
+                                            <option value="Maharastra">Maharashtra</option>
+                                            <option value="Mumbai North-East">Mumbai North-East</option>
+                                            <option value="Mumbai North-central">Mumbai North-central</option>
+                                            <option value="Mumbai North-west">Mumbai North West</option>
+                                            <option value="Mumbai South-central">Mumbai South-central</option>
+                                            <option value="Mumbai South">Mumbai South</option>
+                                        </select>
+                                    </div>
             </div>
             <hr className="line" />
             <div className="locationFilter">
@@ -27,5 +53,6 @@ const MpList = () => (
    }}
      </ListContext.Consumer>
     )
+}
 
 export default MpList
