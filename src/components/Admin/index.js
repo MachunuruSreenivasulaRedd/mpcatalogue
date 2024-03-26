e4ccaf035453a800ef2facf823a37a97937b089c
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2024-03-26 15:29:45 +0530

    changesadded

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -2,8 +2,12 @@ import React, {useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import './index.css'
 import ListContext from '../../context/ListContext'
+import { Link } from 'react-router-dom'
 import { GiHamburgerMenu } from "react-icons/gi";
 
+const stateConstituency = [{"id":"1","name":"Andhra Pradesh", "constituencies":"25"},{"id":"2","name":"Arunachal Pradesh", "constituencies":"2"},{"id":"3","name":"Assam", "constituencies":"14"},{"id":"4","name":"Bihar", "constituencies":"40"},{"id":"5","name":"Chattisgarh", "constituencies":"11"},{"id":"6","name":"Goa", "constituencies":"2"},{"id":"7","name":"Gujarat", "constituencies":"26"},{"id":"8","name":"Haryana", "constituencies":"10"},{"id":"9","name":"Himachal Pradesh", "constituencies":"4"},{"id":"10","name":"Jharkhand", "constituencies":"14"},{"id":"11","name":"Karanataka", "constituencies":"28"},{"id":"12","name":"Kerala", "constituencies":"20"},{"id":"13","name":"Madhya Pradesh", "constituencies":"29"},{"id":"14","name":"Maharashtra", "constituencies":"48"},{"id":"15","name":"Manipur", "constituencies":"2"},
+{"id":"16","name":"Meghalaya", "constituencies":"2"},{"id":"17","name":"Mizoram", "constituencies":"1"},{"id":"18","name":"Nagaland", "constituencies":"1"},{"id":"19","name":"Odisha", "constituencies":"21"},{"id":"20","name":"Punjab", "constituencies":"13"},{"id":"21","name":"Rajasthan", "constituencies":"25"},{"id":"22","name":"Sikkim", "constituencies":"1"},{"id":"23","name":"Tamilnadu", "constituencies":"39"},{"id":"24","name":"Telangana", "constituencies":"17"},{"id":"25","name":"Tripura", "constituencies":"2"},{"id":"26","name":"Uttar Pradesh", "constituencies":"80"},{"id":"27","name":"Uttarakhand", "constituencies":"5"},{"id":"28","name":"West Bengal", "constituencies":"42"},{"id":"29","name":"Andamon and Nicobar Islands", "constituencies":"1"},{"id":"30","name":"Chandigarh", "constituencies":"1"},{"id":"31","name":"Dadra and Nagar Haveli and Daman and Diu", "constituencies":"1"},
+{"id":"32","name":"Lakshadweep", "constituencies":"1"},{"id":"33","name":"Delhi", "constituencies":"7"},{"id":"34","name":"Puducherry", "constituencies":"1"},]
 const constituencies=["North Mumbai","Mumbai North-East","Mumbai-North-Central","Mumbai-North-West","Mumbai-South-Central","Mumbai South"]
 const states=["Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala" ,"madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamilnadu","Telangana","Tripura","UttarPradesh","Uttarakhand","WestBengal","Andaman and NicobarIslands","chandigarh","Lakshadweeep","Dadra and Nagar Haveli and Daman and Diu","Delhi","puducherry","Ladakh"]
 function Admin() {
@@ -23,6 +27,7 @@ function Admin() {
     const [showList,setList]= useState(true)
     const [filteredList,setFilteredList]=useState([])
     const [showmenu,setMenu]=useState(true)
+    const [showStates,setShowStates]=useState(false)
     const addCandidate = () => {
         setCandidate(!isAdd)
         setList(false)
@@ -35,6 +40,9 @@ function Admin() {
     }
     const toggleMenubar = () => {
         setMenu(!showmenu)
+    }
+    const showStatesList = () => {
+        setShowStates(!showStates)
     }
     return (
         <ListContext.Consumer>
@@ -72,24 +80,40 @@ function Admin() {
                                 <h1 className='highLighter'>LOK SABHA <br/>Candidates List</h1>
                                 <div className='selectors'>
                             <div className='input'>
-                                        <label htmlFor="stateName" className='selection'>Select State</label>
                                         <select value={state} id="stateName" onChange={e => setState(e.target.value)}>
+                                        <option selected="selected" disabled="disabled" id="stateHeader">Select State</option>
                                             {states.map(item => (
                                                 <option value={item}>{item}</option>
                                             ))}
                                         </select>
                                     </div>
                                     <div className='input'>
-                                        <label htmlFor='constituencyName' className='selection'>select constituency</label>
-                                        <select value={constituency} id="constituencyName" onChange={e => setconstituency(e.target.value)}>
+                                    
+                                        <select id="constituencyName" onChange={e => setconstituency(e.target.value)}>
+                                        <option selected="selected" disabled="disabled" id="constituencyHeader">Select Constituency</option>
                                             {constituencies.map(item => (
                                             <option value={constituency}>{item}</option>
                                             ))}
                                         </select>
                                     </div>
                                     <button  className='goBtn' type="button" onClick={e=> getCandidates(e)}>Go </button>
+                                    <button onClick={()=>showStatesList()} type="button" className='showstatebtn'>States</button>
                                     <p><span className='spanner'>State:</span> {state}<br/> <span className='spanner'>Constituency:</span> {constituency}</p>
                                     </div>
+                                    </div>
+                                    <div className={showStates ? 'showStates': 'hideStates'}>
+                                        <div className='sectionStates'>
+                                            <div className="stateName">State/Union Territory</div>
+                                            <div className='noconstituencies'>Number of Constituencies</div>
+                                    </div>
+                                    {stateConstituency.map(item => (
+                                        <Link to="/${item.id}" style={{"text-decoration": "none"}}>
+                                            <div className='state'>
+                                            <p id='stateUTName'>{item.name}</p>
+                                            <p className='noConstituencies'>{item.constituencies}</p>
+                                            </div>
+                                        </Link>
+                                    ))}
                                     </div>
                                     <div className='sections'>
                                         <div className='section'>Member</div>
