0f062f8dfa3458accf26a71c56aeb2f3c8980cf8
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2024-03-31 13:05:45 +0530

    changesadded

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import ListContext from '../../context/ListContext'
 import { Link } from 'react-router-dom'
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaArrowLeftLong } from "react-icons/fa6";
 
 const stateConstituency = [{id:1,"name":"Andhra Pradesh", "constituencies":"25"},{"id":2,"name":"Arunachal Pradesh", "constituencies":"2"},{"id":3,"name":"Assam", "constituencies":"14"},{"id":4,"name":"Bihar", "constituencies":"40"},{"id":5,"name":"Chattisgarh", "constituencies":"11"},{"id":6,"name":"Goa", "constituencies":"2"},{"id":7,"name":"Gujarat", "constituencies":"26"},{"id":8,"name":"Haryana", "constituencies":"10"},{"id":9,"name":"Himachal Pradesh", "constituencies":"4"},{"id":10,"name":"Jharkhand", "constituencies":"14"},{"id":11,"name":"Karanataka", "constituencies":"28"},{"id":12,"name":"Kerala", "constituencies":"20"},{"id":13,"name":"Madhya Pradesh", "constituencies":"29"},{"id":14,"name":"Maharashtra", "constituencies":"48"},{"id":15,"name":"Manipur", "constituencies":"2"},
 {"id":16,"name":"Meghalaya", "constituencies":"2"},{"id":17,"name":"Mizoram", "constituencies":"1"},{"id":18,"name":"Nagaland", "constituencies":"1"},{"id":19,"name":"Odisha", "constituencies":"21"},{"id":20,"name":"Punjab", "constituencies":"13"},{"id":21,"name":"Rajasthan", "constituencies":"25"},{"id":22,"name":"Sikkim", "constituencies":"1"},{"id":23,"name":"Tamilnadu", "constituencies":"39"},{"id":24,"name":"Telangana", "constituencies":"17"},{"id":25,"name":"Tripura", "constituencies":"2"},{"id":26,"name":"Uttar Pradesh", "constituencies":"80"},{"id":27,"name":"Uttarakhand", "constituencies":"5"},{"id":28,"name":"West Bengal", "constituencies":"42"},{"id":29,"name":"Andamon and Nicobar Islands", "constituencies":"1"},{"id":30,"name":"Chandigarh", "constituencies":"1"},{"id":31,"name":"Dadra and Nagar Haveli and Daman and Diu", "constituencies":"1"},
@@ -679,6 +680,7 @@ function Admin(){
                                     </table>
                                     </div>
                                     <div className={stateDetails.length>0 ? 'showStateDetails':'hideStateDetails'}>
+                                        <button className='backBtn' type="button" onClick={()=>getCandidates()}><FaArrowLeftLong className='arrow'/>Back</button>
                                         <table className='stateDetails'>      
                                             <tbody>
                                                 <tr>
